323102da5d3489f4a3a506cbfab596d8bc8c9d5b
klouw2010 <klouw2010.edsa@gmail.com>
2020-02-12 21:03:42 +0200

    Added functionspractice file

---
+++ functions/functions/functionspractice.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+
+#Function 1: Metric Dictionary
+
+### START FUNCTION
+def dictionary_of_metrics(items):
+    # your code here
+    return
+
+### END FUNCTION
+
+
+
+#Function 2: Five Number Summary
+
+### START FUNCTION
+def five_num_summary(items):
+    # your code here
+    return
+
+### END FUNCTION
+
+
+
+#Function 3: Date Parser
+
+### START FUNCTION
+def date_parser(dates):
+    # your code here
+    return
+
+### END FUNCTION
+
+
+
+#Function 4: Municipality & Hashtag Detector
+
+### START FUNCTION
+def extract_municipality_hashtags(df):
+    # your code here
+    return
+
+### END FUNCTION
+
+
+
+#Function 5: Number of Tweets per Day
+
+### START FUNCTION
+def number_of_tweets_per_day(df):
+    # your code here
+    return
+
+### END FUNCTION
+
+
+
+#Function 6: Word Splitter
+
+### START FUNCTION
+def word_splitter(df):
+    # your code here
+    return
+
+### END FUNCTION
+
+
+#Function 7: Stop Words
+### START FUNCTION
+def stop_words_remover(df):
+    # your code here
+    return
+
+### END FUNCTION
